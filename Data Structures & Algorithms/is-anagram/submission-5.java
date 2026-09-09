4c710638ef4163f8bba9d512472a41edb2530946
preethisonam <52007335+preethisonam@users.noreply.github.com>
2026-09-10 00:53:06 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] ch = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            ch[s.charAt(i)-'a']++;
+            ch[t.charAt(i)-'a']--;
+
+        }
+        for(int v:ch){
+            if(v!=0){
+                return false;
+            }
+        }
+return true;
+
+    }
+}
